9deac9be3fa55fc4b3a2b9fcf751886b9cc9df71
Lozy <myairski@gmail.com>
2013-04-27 11:32:31 +0800

    Update debian-dante.sh

--- debian-dante.sh
+++ debian-dante.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 #For Debian
 
+serverip=$(grep $(hostname) /etc/hosts| awk '{print $1}')
+
+if [ "$serverip" -eq "127.0.0.1" ] || [ "$serverip" -eq "" ] ;then
+  serverip=$(ifconfig venet0:0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " ")
+fi
+
+echo "Auto Generate Your Ip address".$serverip
+read -p "Corret it? Or press Enter" newip
+read -p "Sock 5 Port[1024-65535]: (Default 9200)" newport
+
+if [ "$newip" -ne "" ];then serverip=newip
+fi
+
+if [ "$newport" -eq "" ];then newport=9200
+fi
+
+
 apt-get update
 apt-get install nano dante-server -y
 
-serverip=`ifconfig venet0:0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " "`
-
 cat >/etc/danted.conf<<EOF
-internal: $serverip  port =  2013
+internal: $serverip  port = $newport
 external: $serverip
 method: username none
 logoutput: /var/log/danted.log
@@ -37,15 +52,10 @@ log: connect error
 }
 EOF
 
-cat >> ~/.bashrc<<EOF
-alias s5='/etc/init.d/danted start'
-alias kills5='/etc/init.d/danted stop'
-EOF
-
 /etc/init.d/danted start
 rm debian-dante.sh -f
 cat >>/var/spool/cron/root<<EOF
-0 0 * * * /etc/init.d/danted restart
+0 0 * * * killall danted;rm -rf /var/log/daned.log;/etc/init.d/danted start > /dev/null 2>&1
 EOF
 crontab -u root /var/spool/cron/root
 exit
