64181ef413d6fe8a214dfcd4bbd056cfc7df4c4d
Lozy <myairski@gmail.com>
2014-01-31 15:07:06 +0800

    Update centos-dante.sh
    1. Upgrade to the latest version of danted,1.4.0.
    2. Enable Pam user authentication(default).
    3. Multiple IP of server detected. 
    4. Create danted daemon support multiple config file(Path: /etc/danted/conf/*.conf). 
    5. Create default user: sock and pam user: sock5(sock5).
    6. Auto start process when reboot. 
    
    -------------------------------------
    1. 升级到danted最新版本的1.4.0，注意，1.4.0 相比之前，配置文件有变化。
    2. 开启Pam认证，编译了pam_pwdfile模块，支持非系统用户认证。
    3. 多IP识别，回车添加全部IP/定义指定IP。
    4. 参照Debian源的danted Daemon,在原有的重启，停止功能上，添加了加载多配置文件（/etc/danted/conf/*.conf）、状态查看，Pam用户添加/删除。
    5. 默认自动配置，添加系统运行用户sock 以及PAM默认用户 sock5(密码sock5)
    6. 添加自动重启功能。

--- centos-dante.sh
+++ centos-dante.sh
@@ -1,76 +1,270 @@
 #!/bin/sh
 #For Centos
-
-serverip=$(grep $(hostname) /etc/hosts| awk '{print $1}')
-
-if [ "$serverip" -eq "127.0.0.1" ] || [ "$serverip" -eq "" ] ;then
-	serverip=$(ifconfig venet0:0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " ")
-fi
-
-echo "Auto Generate Your Ip address".$serverip
-read -p "Corret it? Or press Enter" newip
-read -p "Sock 5 Port: (Default 9200)" newport
-
-if [ "$newip" -ne "" ];then serverip=newip
-fi
-
-if [ "$newport" -eq "" ];then newport=9200
-fi
-
-yum update -y
-yum install gcc make -y
-
-wget ftp://ftp.inet.no/pub/socks/dante-1.3.2.tar.gz
-tar zxvf dante*
-cd dante*
-./configure
-make && make install
-
-cat >/etc/danted.conf<<EOF
-internal: $serverip  port = $newport
-external: $serverip
-method: username none
-logoutput: /var/log/danted.log
+DEFAULT_PORT="2014"
+DEFAULT_USER="sock5"
+DEFAULT_PAWD="sock5"
+VERSION="v1.4.0"
+genconfig(){
+  # CONFIGFILE $IP $PORT $N
+  CONFIGFILE=$1
+  IP=$2
+  PORT=$3
+  num=$4
+  cat >$CONFIGFILE<<EOF
+internal: ${IP} port = ${PORT}
+external: ${IP}
+#socksmethod: username none
+socksmethod: pam.username
+user.notprivileged: sock
+logoutput: /var/log/danted.${num}.log
 
 client pass {
 from: 0.0.0.0/0 to: 0.0.0.0/0
 log: connect disconnect
 }
-pass {
+socks pass {
 from: 0.0.0.0/0 to: 0.0.0.0/0 port gt 1023
 command: bind
 log: connect disconnect
 }
-pass {
+socks pass {
 from: 0.0.0.0/0 to: 0.0.0.0/0
 command: connect udpassociate
 log: connect disconnect
 }
-pass {
+socks pass {
 from: 0.0.0.0/0 to: 0.0.0.0/0
 command: bindreply udpreply
 log: connect error
 }
-block {
+socks block {
 from: 0.0.0.0/0 to: 0.0.0.0/0
 log: connect error
 }
 EOF
+}
 
-cat >> ~/dante<<EOF
-#!/bin/bash
+yum install gcc g++ pam-devel tcp_wrappers-devel make unzip httpd-tools -y
+useradd sock -s /bin/false > /dev/null 2>&1
+#echo sock:sock | chpasswd
+mkdir -p /etc/danted/conf
 
-killall danted
-killall sockd
-rm -rf /var/log/danted.log
+Getserverip_n=$(ifconfig | grep 'inet addr' | grep -Ev 'inet addr:127.0.0|inet addr:192.168.0|inet addr:10.0.0' | sed -n 's/.*inet addr:\([^ ]*\) .*/\1/p' | wc -l)
+Getserverip=$(ifconfig | grep 'inet addr' | grep -Ev 'inet addr:127.0.0|inet addr:192.168.0|inet addr:10.0.0' | sed -n 's/.*inet addr:\([^ ]*\) .*/\1/p')
 
-sleep 2
-/usr/local/sbin/sockd -f /etc/danted.conf &
+serverip=$Getserverip
+( [ -z "$serverip" ] || [ -z "$(echo $Getserverip | grep "$serverip" )" ] ) && echo 'Get IP address Error.Try again OR report bug.' && exit
+[ $Getserverip_n -gt 1 ] && echo "$Getserverip" && read -p  "Server IP > 1, Please Input Taget Danted Server IP OR Enter to config all " serverip
+
+if [ -z "$serverip" ];then
+   i=0
+   echo "$Getserverip" | while read theip;do
+      port=$DEFAULT_PORT
+      configfile="/etc/danted/conf/sockd-"$(echo $Getserverip | sed 's/.*\.\(.*\)/\1/g')"${i}.conf"
+   	  genconfig $configfile $theip $port $i
+   	  i=$((i+1))
+   done
+else
+      port=$DEFAULT_PORT
+      configfile="/etc/danted/conf/sockd-"$(echo $serverip | sed 's/.*\.\(.*\)/\1/g')"${i}.conf"
+   	  genconfig $configfile $serverip $port 0
+fi
+
+mkdir tmp
+cd tmp
+
+#start-stop-daemon
+if [ -z "$(command -v start-stop-daemon)" ];then
+wget http://developer.axis.com/download/distribution/apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz
+tar zxvf apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz
+gcc apps/sys-utils/start-stop-daemon-IR1_9_18-2/start-stop-daemon.c -o start-stop-daemon -o /usr/local/sbin/start-stop-daemon
+fi
+#libpam-pwdfile
+if [[ ! -s /lib/security/pam_pwdfile.so ]];then
+wget https://github.com/tiwe-de/libpam-pwdfile/archive/master.zip -O master.zip
+unzip master.zip
+cd libpam-pwdfile-master/
+make && make install
+cd ../
+fi
+
+if [[ ! -s /etc/danted/sbin/sockd ]]  || [ -z "$(/etc/danted/sbin/sockd -v | grep '$VERSION')" ];then
+wget http://www.inet.no/dante/files/dante-1.4.0.tar.gz
+tar zxvf dante*
+cd dante*
+./configure --with-sockd-conf=/etc/danted/conf/sockd.conf --prefix=/etc/danted
+make && make install
+cd ../../
+fi
+#Complile Done
+rm tmp -rf
+
+cat > /etc/pam.d/sockd  <<EOF
+auth required pam_pwdfile.so pwdfile /etc/danted/socks.passwd
+account required pam_permit.so
+EOF
+/usr/bin/htpasswd -c -d -b /etc/danted/socks.passwd ${DEFAULT_USER} ${DEFAULT_PAWD}
+
+cat > /etc/init.d/danted <<'EOF'
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          danted
+# Reprogarm:         airski
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: SOCKS (v4 and v5) proxy daemon (danted)
+### END INIT INFO
+#
+# dante socks5 daemon for Debian/Centos
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON=/etc/danted/sbin/sockd
+DESC="Dante SOCKS daemon"
+CONFIGDIR=/etc/danted/conf
+
+test -f $DAEMON || exit 0
+
+set -e
+
+start_daemon_all(){
+    ls ${CONFIGDIR}/*.conf | while read configed;do
+    	NAME=$(echo $configed | sed 's/.*\/\(.*\)\.conf/\1/g')
+    	PIDFILE=/var/run/$NAME.pid
+    	LOGFILE=$(cat $configed | grep '^logoutput' | sed 's/.*logoutput: \(.*\).*/\1/g')
+
+    	echo -n "  config $NAME "
+    	
+    	if [ -s $PIDFILE ] && [ -n "$( ps aux | awk '{print $2}'| grep "^$(cat $PIDFILE)$" )" ];then
+    	     echo " [ Runing;Failed ] " 
+    	     continue
+    	fi
+
+        echo >$PIDFILE
+        cp /dev/null $LOGFILE
+
+        if ! egrep -cve '^ *(#|$)' \
+	       -e '^(logoutput|user\.((not)?privileged|libwrap)):' $configed > /dev/null
+	    then
+		   echo " [ not configured ]"
+		   continue
+	    fi
+        start-stop-daemon --start --quiet --background --oknodo --pidfile $PIDFILE \
+		--exec $DAEMON -- -f $configed -p $PIDFILE
+		( [ -s $PIDFILE ] && echo " [ Runing ]" ) || echo " [ Faild ]"
+    done
+}
+stop_daemon_all(){
+    ls ${CONFIGDIR}/*.conf | while read configed;do
+    	NAME=$(echo $configed | sed 's/.*\/\(.*\)\.conf/\1/g')
+    	PIDFILE=/var/run/$NAME.pid
+        echo -n "  config $NAME "
+        if [[ ! -s $PIDFILE ]];then 
+          echo " [ PID.LOST;Unable ]" 
+          continue
+        fi
+        start-stop-daemon --stop --quiet --oknodo --pidfile $PIDFILE \
+		--exec $DAEMON -- -f $configed -p $PIDFILE
+		( [ -n "$( ps aux | awk '{print $2}'| grep "^$(cat $PIDFILE)$" )" ] && \
+			echo " [ Failed ]" ) || echo " [ Stop ]"
+    done
+}
+reload_daemon_all(){
+    ls ${CONFIGDIR}/*.conf | while read configed;do
+    	NAME=$(echo $configed | sed 's/.*\/\(.*\)\.conf/\1/g')
+    	PIDFILE=/var/run/$NAME.pid
+        echo -n "  config $NAME "
+        if [ -s $PIDFILE ];then
+        	if [ -z "$( ps aux | awk '{print $2}'| grep "^$(cat $PIDFILE)$" )" ];then
+        	  echo " [ PID.DIE;Unable ]"  
+        	  continue
+        	fi
+       else
+            echo " [ PID.LOST;Unable ]" 
+            continue
+        fi
+        start-stop-daemon --stop --signal 1 --quiet --oknodo --pidfile $PIDFILE \
+		--exec $DAEMON -- -f $configed -p $PIDFILE
+		( [ -n "$( ps aux | awk '{print $2}'| grep "^$(cat $PIDFILE)$" )" ] && echo " [ Runing ]" ) || echo " [ Failed ]"
+    done
+}
+status_daemon_all(){
+	ls ${CONFIGDIR}/*.conf | while read configed;do
+		NAME=$(echo $configed | sed 's/.*\/\(.*\)\.conf/\1/g')
+    	PIDFILE=/var/run/$NAME.pid
+        echo -n "  config $NAME " $(cat $configed | grep '^internal' | sed 's/internal:\(.*\)port.*=\(.*\)/\1:\2/g' | sed 's/ //g')"  "
+        if [[ ! -s $PIDFILE ]];then
+        	echo " [ Stop ]"
+        	continue
+        fi
+        ( [ -n "$( ps aux | awk '{print $2}'| grep "^$(cat $PIDFILE)$" )" ] && echo " [ Runing ]" ) || echo " [ PID.DIE;Unable ]"
+    done
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>"
+    echo "  Active User:" $(cat /etc/danted/socks.passwd | while read line;do echo $line| sed 's/\(.*\):.*/\1/';done) 
+}
+add_user(){
+	User=$1
+	Passwd=$2
+	( [ -z "$User" ] || [ -z "$Passwd" ] ) && echo " Error: User or password can't be blank" && return 0 
+    /usr/bin/htpasswd -d -b /etc/danted/socks.passwd ${User} ${Passwd}
+}
+del_uer(){
+   	User=$1
+	[ -z "$User" ] && echo " Error: User Name can't be blank" && return 0 
+    /usr/bin/htpasswd -D /etc/danted/socks.passwd ${User}
+}
+case "$1" in
+  start)
+	echo "Starting $DESC: "
+	start_daemon_all
+	;;
+  stop)
+	echo "Stopping $DESC: "
+	stop_daemon_all
+	;;
+  reload)
+	echo "Reloading $DESC configuration files."
+	reload_daemon_all
+  ;;
+  restart)
+	echo "Restarting $DESC: "
+	stop_daemon_all
+	sleep 1
+	start_daemon_all
+	;;
+  status)
+    echo "Curent Status Of $DESC: "
+    status_daemon_all
+    ;;
+  add)
+    echo "Adding User For $DESC: "
+    add_user "$2" "$3"
+    ;;
+  del)
+    echo "Clearing User For $DESC: "
+    del_user "$2"
+    ;;
+  *)
+	N=/etc/init.d/danted
+	echo "Usage: $N {start|stop|restart|status|add|del}" >&2
+	exit 1
+	;;
+esac
 
 exit 0
 EOF
-
-/usr/local/sbin/sockd -f /etc/danted.conf &
-rm dante-1.3.2* -rf
-rm centos-dante.sh -rf
-exit
+chmod +x /etc/init.d/danted
+chkconfig --add danted
+ln -s /etc/danted/sbin/sockd /usr/bin/danted
+service danted start
+clear
+cat <<EOF
+Danted Auto Config 
+*************************************
+SOCK5 ${Getserverip}
+PORT  ${DEFAULT_PORT}
+Auth  ${DEFAULT_USER}:${DEFAULT_PAWD}
+*************************************
+EOF
+exit 0
