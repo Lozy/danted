9a9364035e29b5239d411cbc29a7747270f9a4c1
Lozy <lozy@sockd.info>
2017-01-17 19:59:37 +0800

    adding New script

---
+++ script/debian.sh
@@ -0,0 +1,310 @@
+#!/bin/bash
+#
+#   wget -q --no-check-certificate https://script.nie.link/sockd.sh -O sockd.sh
+#
+
+VERSION="1.3.2"
+INSTALL_FROM="compile"
+DEFAULT_PORT="2016"
+DEFAULT_USER=""
+DEFAULT_PAWD=""
+WHITE_LIST_NET=""
+WHITE_LIST=""
+SCRIPT_HOST="https://public.sockd.info"
+PACKAGE_NAME="dante_1.3.2-1_$(uname -m).deb"
+COLOR_PATH="/etc/default/color"
+
+BIN_DIR="/etc/danted"
+BIN_PATH="/etc/danted/sbin/sockd"
+CONFIG_PATH="/etc/danted/sockd.conf"
+BIN_SCRIPT="/etc/init.d/sockctl"
+
+DEFAULT_IPADDR=$(ip addr | grep 'inet ' | grep -Ev 'inet 127|inet 192\.168|inet 10\.' | \
+            sed "s/[[:space:]]*inet \([0-9.]*\)\/.*/\1/")
+RUN_PATH=$(cd `dirname $0`;pwd )
+
+##################------------Func()---------#####################################
+detect_install(){
+    if [ -s "${BIN_PATH}" ];then
+        echo "dante socks5 already install"
+        ${BIN_PATH} -v
+    fi
+}
+
+generate_config_ip(){
+    local ipaddr="$1"
+    local port="$2"
+
+    cat <<EOF
+# Generate interface ${ipaddr}
+internal: ${ipaddr}  port = ${port}
+external: ${ipaddr}
+EOF
+}
+
+generate_config_iplist(){
+    local ipaddr_list="$1"
+    local port="$2"
+
+    [ -z "${ipaddr_list}" ] && return 1
+    [ -z "${port}" ] && return 2
+
+    for ipaddr in ${ipaddr_list};do
+        generate_config_ip ${ipaddr} ${port} >> ${CONFIG_PATH}
+    done
+
+    if [ ${#ipaddr_list[@]} -gt 1 ];then
+        echo "external.rotation: same-same" >> ${CONFIG_PATH}
+    fi
+}
+
+generate_config_static(){
+    cat <<EOF
+method: pam none
+clientmethod: none
+user.privileged: root
+user.notprivileged: sock
+logoutput: /var/log/danted.log
+
+client pass {
+        from: 0.0.0.0/0  to: 0.0.0.0/0
+}
+client block {
+        from: 0.0.0.0/0 to: 0.0.0.0/0
+}
+EOF
+}
+generate_config_white(){
+    local white_ipaddr="$1"
+
+    [ -z "${white_ipaddr}" ] && return 1
+
+    # x.x.x.x/32
+    for ipaddr_range in ${white_ipaddr};do
+        cat <<EOF
+#------------ Network Trust: ${ipaddr_range} ---------------
+pass {
+        from: ${ipaddr_range} to: 0.0.0.0/0
+        method: none
+}
+
+EOF
+    done
+}
+
+generate_config_whitelist(){
+    local whitelist_url="$1"
+
+    if [ -n "${whitelist_url}" ];then
+        ipaddr_list=$(curl -s --insecure -A "Mozilla Server Init" ${whitelist_url})
+        generate_config_white "${ipaddr_list}"
+    fi
+}
+
+generate_config_bottom(){
+    cat <<EOF
+pass {
+        from: 0.0.0.0/0 to: 0.0.0.0/0
+        protocol: tcp udp
+        method: pam
+        log: connect disconnect
+}
+block {
+        from: 0.0.0.0/0 to: 0.0.0.0/0
+        log: connect error
+}
+
+EOF
+}
+
+generate_config(){
+    local ipaddr_list="$1"
+    local whitelist_url="$2"
+    local whitelist_ip="$3"
+
+    mkdir -p ${BIN_DIR}
+
+    echo "# Generate by sockd.info" > ${CONFIG_PATH}
+
+    generate_config_iplist "${ipaddr_list}" ${DEFAULT_PORT} >> ${CONFIG_PATH}
+
+    generate_config_static >> ${CONFIG_PATH}
+    generate_config_white ${whitelist_ip} >> ${CONFIG_PATH}
+    generate_config_whitelist "${whitelist_url}" >> ${CONFIG_PATH}
+    generate_config_bottom  >> ${CONFIG_PATH}
+}
+
+download_file(){
+    local path="$1"
+    local filename="$2"
+    local execute="$3"
+
+    [ -z "${filename}" ] && filename="$path"
+
+    [ -n "$path" ] && \
+        wget -q --no-check-certificate ${SCRIPT_HOST}/${path} -O ${filename}
+
+    [ -f "${filename}" ] && [ -n "${execute}" ] && chmod +x ${filename}
+}
+
+##################------------Menu()---------#####################################
+for _PARAMETER in $*
+do
+    case "${_PARAMETER}" in
+      --ip=*)   #split in ; ip1;ip2;
+        ipaddr_list=$(echo "${_PARAMETER#--ip=}" | sed 's/;/\n/g' | sed '/^$/d')
+      ;;
+      --port=*)
+        port="${_PARAMETER#--port=}"
+      ;;
+      --user=*)
+        user="${_PARAMETER#--user=}"
+      ;;
+      --passwd=*)
+        passwd="${_PARAMETER#--passwd=}"
+      ;;
+      --whitelist=*)
+        whitelist_ipaddrs=$(echo "${_PARAMETER#--whitelist=}" | sed 's/;/\n/g' | sed '/^$/d')
+      ;;
+      --whitelist-url=*)
+        whitelist="${_PARAMETER#--whitelist-url=}"
+      ;;
+      --from-package)
+        INSTALL_FROM="package"
+      ;;
+      --update-whitelist|-u)
+        install_ctl
+        gen_config_only="True"
+      ;;
+      --force-update|-f)
+        [ -s "${BIN_SCRIPT}" ] && ${BIN_SCRIPT} stop > /dev/null 2>&1
+        [ -f "${BIN_SCRIPT}" ] && rm "${BIN_SCRIPT}"
+        [ -n "$BIN_DIR" ] && rm -r "$BIN_DIR"
+      ;;
+      --help|-h)
+        clear
+        options=(
+                  "--ip=@Socks5 Server Ip address" \
+                  "--port=[${DEFAULT_PORT}]@port for dante socks5 server" \
+                  "--user=@Socks5 Auth user" \
+                  "--passwd=@Socks5 Auth passwd"\
+                  "--master=@Socks5 Atuth IP" \
+                  "--whitelist=@Socks Auth whitelist" \
+                  "--from-package@Install package from Bin package" \
+                  "--update-whitelist | -u @update white list" \
+                  "--force-update | -f @force update sockd" \
+                  "--help,-h@print help info" )
+        printf "Usage: %s [OPTIONS]\n\nOptions:\n\n" $0
+
+        for option in "${options[@]}";do
+          printf "  %-20s%s\n" "$( echo ${option} | sed 's/@.*//g')"  "$( echo ${option} | sed 's/.*@//g')"
+        done
+        echo -e "\n"
+        exit 1
+      ;;
+      *)
+          exit 1
+      ;;
+
+    esac
+done
+
+[ -n "${port}" ] && DEFAULT_PORT="${port}"
+[ -n "${ipaddr_list}" ] && DEFAULT_IPADDR="${ipaddr_list}"
+[ -n "${user}" ] && DEFAULT_USER="${user}"
+[ -n "${passwd}" ] && DEFAULT_PAWD="${passwd}"
+[ -n "${whitelist_ipaddrs}" ] && WHITE_LIST_NET="${whitelist_ipaddrs}"
+[ -n "${whilelist}" ] && WHITE_LIST="${whilelist}"
+
+generate_config "${DEFAULT_IPADDR}" "${WHITE_LIST}"
+
+[ -n "$gen_config_only" ]  && echo "===========>> update config" && cat ${CONFIG_PATH} && exit 0
+
+download_file "script/sockctl" "${BIN_SCRIPT}" "execute"
+
+[ -n "$(detect_install)" ] && echo "dante sockd already install." && exit 1
+[ -n "$COLOR_PATH" ] && [ ! -s "$COLOR_PATH" ] && download_file "script/color" $COLOR_PATH && . $COLOR_PATH
+
+########################################## DEBIAN 8 #####################################################################
+apt-get update
+apt-get install gcc g++ make vim libpam-dev libwrap0-dev unzip apache2-utils -y
+
+mkdir -p /tmp/danted && rm /tmp/danted/* -rf
+cd /tmp/danted
+
+useradd sock -s /bin/false > /dev/null 2>&1
+
+#--# Check libpam-pwdfile
+if [ ! -s /lib/security/pam_pwdfile.so ];then
+    download_file "source/libpam-pwdfile.zip" "libpam-pwdfile.zip"
+    if [ -f "libpam-pwdfile.zip" ];then
+        unzip libpam-pwdfile.zip
+        cd libpam-pwdfile-master && make && make install
+        cd ../
+    fi
+fi
+
+if [ "$INSTALL_FROM" == "compile" ];then
+    download_file "source/dante-${VERSION}.tar.gz" "dante-${VERSION}.tar.gz"
+
+    if [ -f "dante-${VERSION}.tar.gz" ];then
+        tar zxvf dante*
+        cd dante* && ./configure --with-sockd-conf=${CONFIG_PATH} --prefix=${BIN_DIR}
+        make && make install
+    fi
+else
+    download_file "source/${PACKAGE_NAME}" "${PACKAGE_NAME}"
+    [ -f "${PACKAGE_NAME}" ] && mkdir ${BIN_DIR} && dpkg -i ${PACKAGE_NAME}
+fi
+
+cat > /etc/pam.d/sockd  <<EOF
+auth required pam_pwdfile.so pwdfile ${BIN_DIR}/sockd.passwd
+account required pam_permit.so
+EOF
+
+cat > /etc/default/sockd <<EOF
+# Default Config for sockd
+# -n :: not tcp-keep alive
+Start_Process=1
+Sockd_Opts="-n"
+EOF
+
+rm /usr/bin/sockd -f && ln -s /etc/danted/sbin/sockd /usr/bin/sockd
+${BIN_SCRIPT} adduser "${DEFAULT_USER}" "${DEFAULT_PAWD}"
+
+if [ -n "$(ls -l /sbin/init | grep systemd)" ];then
+    download_file "script/sockctl.service" "/lib/systemd/system/sockctl.service"
+    systemctl enable sockctl
+else
+    update-rc.d sockctl defaults
+fi
+
+clear
+service sockctl restart
+
+
+if [ -n "$(netstat -atn | grep "$DEFAULT_PORT")" ];then
+    cat <<EOF
+${CCYAN}+-----------------------------------------+$CEND
+${CGREEN} Dante Socks5 Install Done. $CEND
+${CCYAN}+-----------------------------------------+$CEND
+${CGREEN} Dante Version:       $CMAGENTA v${VERSION}$CEND
+${CGREEN} Socks5 Info:         $CMAGENTA$CEND
+EOF
+
+    for ipaddr in ${DEFAULT_IPADDR};do
+        echo "${CGREEN}                      $CMAGENTA ${ipaddr}:${DEFAULT_PORT}$CEND"
+    done
+
+    cat <<EOF
+${CGREEN} Socks5 User&Passwd:  $CMAGENTA ${DEFAULT_USER}:${DEFAULT_PAWD}$CEND
+${CCYAN}+_________________________________________+$CEND
+EOF
+    echo -e "\033[32m Dante Server Install Successfuly! \033[0m"
+else
+    echo -e "\033[1;31m Dante Server Install Failed! \033[0m"
+fi
+
+${BIN_SCRIPT} -h
+
+exit 0
