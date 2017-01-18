aedf0b4d1a6f2dcaaea50f6794c30148abc9b0dd
Lozy <lozy@sockd.info>
2017-01-18 17:07:17 +0800

    adding script server for backup

--- install.sh
+++ install.sh
@@ -6,16 +6,21 @@
 #   -- Author:      Lozy
 #
 
+REQUEST_SERVER="https://raw.github.com/Lozy/danted/master"
+SCRIPT_SERVER="https://public.sockd.info"
+
+[ "$1" == "--no-git" ] && REQUEST_SERVER=${SCRIPT_SERVER}
+
 if [ -s "/etc/os-release" ];then
     os_name=$(sed -n 's/PRETTY_NAME="\(.*\)"/\1/p' /etc/os-release)
 
     if [ -n "$(echo ${os_name} | grep -Ei 'Debian|Ubuntu' )" ];then
         printf "Current OS: %s\n" "${os_name}"
-        wget -qO- --no-check-certificate https://raw.github.com/Lozy/danted/master/install_debian.sh | \
+        wget -qO- --no-check-certificate ${REQUEST_SERVER}/install_debian.sh | \
             bash -s -- $@
     elif [ -n "$(echo ${os_name} | grep -Ei 'CentOS')" ];then
         printf "Current OS: %s\n" "${os_name}"
-        wget -qO- --no-check-certificate https://raw.github.com/Lozy/danted/master/install_centos.sh | \
+        wget -qO- --no-check-certificate ${REQUEST_SERVER}/install_centos.sh | \
             bash -s -- $@
     else
         printf "Current OS: %s is not support.\n" "${os_name}"
