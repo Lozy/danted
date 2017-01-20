2bf7f62cd3f87e184b69bb843183186453b9dc17
Lozy <lozy@sockd.info>
2017-01-20 08:04:36 +0800

    adding Start-Stop Daemon for CentOS

--- install_centos.sh
+++ install_centos.sh
@@ -248,6 +248,14 @@ mkdir -p /tmp/danted && rm /tmp/danted/* -rf && cd /tmp/danted
 
 id sockd > /dev/null 2>&1 || useradd sockd -s /bin/false
 
+# Installing Start-Stop Daemon
+if [ -z "$(command -v start-stop-daemon)" ];then
+    download_file "source/apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz" "start-stop-daemon-IR1_9_18-2.tar.gz"
+    tar zxvf start-stop-daemon-IR1_9_18-2.tar.gz
+    gcc apps/sys-utils/start-stop-daemon-IR1_9_18-2/start-stop-daemon.c \
+        -o start-stop-daemon -o /usr/local/sbin/start-stop-daemon
+fi
+
 #--# Check libpam-pwdfile
 if [ ! -s /lib/security/pam_pwdfile.so ];then
     download_file "source/libpam-pwdfile.zip" "libpam-pwdfile.zip"
